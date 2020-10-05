10ff3b9c4de89a01a8967a47d424c3ba0ecc29c9
Madeleine Bizard <bizard.madeleine@gmail.com>
2020-10-05 17:42:10 +0100

    model, controller, routes and view for furnitures

---
+++ app/controllers/furnitures_controller.rb
@@ -0,0 +1,35 @@
+class FurnituresController < ApplicationController
+
+  def new
+    @furniture = Furniture.new
+  end
+
+  def create
+    @furniture = Furniture.new(furniture_params)
+    @furniture.save
+    redirect_to furniture_path(@furniture)
+  end
+
+  def edit
+    @furniture = Furniture.find(params[:id])
+  end
+
+  def update
+    @furniture = Furniture.find(params[:id])
+    @furniture.update(furniture_params)
+    redirect_to furniture_path(@furniture)
+  end
+
+  def destroy
+    @furniture = Furniture.find(params[:id])
+    @furniture.destroy
+    redirect_to furniture_path
+  end
+
+  private
+
+  def furniture_params
+    params.require(:furniture).permit(:name, :description, :price, :measure, :category)
+  end
+
+end
