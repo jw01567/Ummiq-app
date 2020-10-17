e0db4fd8be7c502152af7cbb29333d908882857e
jules welcomme <jules.welcomme@gmail.com>
2020-10-17 16:16:45 +0100

    accessories controller

---
+++ app/controllers/accessories_controller.rb
@@ -0,0 +1,44 @@
+class AccessoriesController < ApplicationController
+  def index
+    @accessories = Accessory.all
+  end
+
+  def show
+    @accessory = Accessory.find(params[:id])
+  end
+
+  def new
+    @accessory = Accessory.new
+  end
+
+  def create
+    @accessory = Accessory.new(accessory_params)
+    if @accessory.save
+      redirect_to accessory_path(@accessory)
+    else
+      render new
+    end
+  end
+
+  def edit
+    @accessory = Accessory.find(params[:id])
+  end
+
+  def update
+    @accessory = Accessory.find(params[:id])
+    @accessory.update(accessory_params)
+    redirect_to accessory_path(@accessory)
+  end
+
+  def destroy
+    @accessory = Accessory.find(params[:id])
+    @accessory.destroy
+    redirect_to accessory_path
+  end
+
+  private
+
+  def accessory_params
+    params.require(:accessory).permit(:name, :description, :price, :measure, :category)
+  end
+end
