84c47d3ba5e00e01d81bd14af4257120274e124d
jules welcomme <jules.welcomme@gmail.com>
2020-10-18 12:40:28 +0100

    designers controller

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,2 +1,43 @@
 class DesignersController < ApplicationController
+  def index
+    @designers = Designer.all
+  end
+
+  def show
+    @designer = Designer.find(params[:id])
+  end
+
+  def new
+    @designer = Designer.new
+  end
+
+  def create
+    @designer = Designer.new
+    if @designer.save
+      redirect_to designers_path
+    else
+      render new
+    end
+  end
+
+  def edit
+    @designer = Designer.find(params[:id])
+  end
+
+  def update
+    @designer = Designer.find(params[:id])
+    @designer.update(designer_params)
+    redirect_to designer_path(@designer)
+  end
+
+  def destroy
+    @designer = Designer.find(params[:id])
+    @designer.destroy
+    redirect_to designer_path
+  end
+
+  def furniture_params
+    params.require(:designer).permit(:name, :description)
+  end
+
 end
